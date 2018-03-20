b4122ad7f95ea479eec8b1b389784f8c39311f34
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-03-20 21:07:48 +0100

    Initial scaffolding

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+    ],
+    link: [],
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/styles/main.scss'],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend(config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
+};
