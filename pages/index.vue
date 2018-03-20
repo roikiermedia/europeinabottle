4f644f34d0adf18f03a364920daf995d78563bfc
scammo <samuel.brinkmann@googlemail.com>
2018-03-20 22:25:35 +0100

    2

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,47 @@
 <template>
   <section>
+
+    <div>
+      <span>Your name:* </span>
+      <input v-model="userName" placeholder="Write your name">
+    </div>
+
+    <div>
+      <span>Where are you from: </span>
+      <input v-model="userLocation" placeholder="Write your location">
+    </div>
+
+    <span>What is your bottle message is:* </span>
+    <p style="white-space: pre-line;">{{ userMessage }}</p>
+    <br>
+    <textarea v-model="userMessage" placeholder="add multiple lines"></textarea>
+
+    <br/>
+    <input type="checkbox" id="checkbox" v-model="checked">* Please check that you are following <a href="#">Our Guidlines</a>
+
+    <br/>
+    <button>Send your bottle away</button>
+
+    <p>
+      You need to fill out your name and a message for sending your bottle away.
+    </p>
+
   </section>
 </template>
 
 <script>
-export default {};
+
+  export default {
+    name: 'index',
+    data() {
+      return {
+        userName: '',
+        userLocation: '',
+        userMessage: '',
+        checked: false,
+      };
+    },
+  };
 </script>
 
 <style scoped>
