ccb00f91a190a6f16af7a5e3897b9d95d000ca8e
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-03-21 11:54:13 +0100

    Added background map to index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,33 @@
 <template>
   <section>
-    <button @click="testpost">test post</button>
-    <button @click="testget">test get</button>
+    <!-- <button @click="testpost">test post</button>
+    <button @click="testget">test get</button> -->
+
+    <mgl-map class="map" :center="center" :zoom="zoom" :mapStyle="style">
+      <mgl-navigation-control position="top-right"/>
+      <mgl-geolocate-control position="top-right"/>
+    </mgl-map>
   </section>
 </template>
 
 <script>
 import axios from '~/plugins/axios';
+import { MglMap, MglNavigationControl, MglGeolocateControl, MglMarker } from 'vue-mapbox';
 
 export default {
+  components: {
+    MglMap,
+    MglNavigationControl,
+    MglGeolocateControl,
+    MglMarker,
+  },
+  data() {
+    return {
+      style: '/mapstyle.json',
+      center: [9.43564, 54.7886],
+      zoom: 14,
+    };
+  },
   methods: {
     testpost() {
       axios.post('/api/message', { message: {
@@ -28,4 +47,9 @@ export default {
 </script>
 
 <style scoped>
+.map {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ plugins/vue-mapbox.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import VueMapbox from 'vue-mapbox';
+import Mapbox from 'mapbox-gl';
+
+Vue.use(VueMapbox, { mapboxgl: Mapbox });
