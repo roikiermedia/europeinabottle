a309b451aec748c4d382eb18bad58fbde809616c
scammo <samuel.brinkmann@googlemail.com>
2018-03-22 12:02:09 +0100

    admin interface works

---
+++ pages/admin/bottles/test.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <section>
+    <h3 v-on:click="counter += 1">List of all Bottles - {{counter}}</h3>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+  };
+</script>
+
+<style scoped>
+</style>
--- server/api/messages.js
+++ server/api/messages.js
@@ -13,13 +13,13 @@ const messages = new Datastore({ filename: 'server/database/messages.db', autolo
 //   }
 
 router.get('/randommessage', (req, res) => {
-  messages.count({}, (err, count) => {
+  messages.count({moderated: true}, (err, count) => {
     if (err) res.status(500);
 
     // count is the number of docs
     // skip a random number between 0 to count-1
     const skipCount = Math.floor(Math.random() * count);
-    messages.find({}).skip(skipCount).limit(1).exec((err2, docs) => {
+    messages.find({moderated: true}).skip(skipCount).limit(1).exec((err2, docs) => {
       if (err2) res.status(500);
 
       // docs[0] is your random doc
@@ -35,10 +35,24 @@ router.get('/bottle/:id', (req, res) => {
   });
 });
 
+router.get('/bottles', (req, res) => {
+  messages.find({ }).exec((err, doc) => {
+    if (err) res.status(500);
+    res.json(doc);
+  });
+});
+
+
 router.post('/bottle', (req, res) => {
   messages.insert(req.body.message, (err, newDoc) => {
     if (err) res.status(500);
     res.json(newDoc);
+  });
+});
+// Update
+router.post('/bottle/:id', (req, res) => {
+  messages.update({ _id: req.params.id }, { $set: req.body.messages}, {}, (err, numReplaced) => {
+    console.log('done');
   });
 });
 
