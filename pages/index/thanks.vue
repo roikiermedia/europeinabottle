7cf1beda059610fe3c82f8ec2ddbe0d6dbe5342f
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-03-22 10:39:30 +0100

    Added thank you text.

--- pages/index/thanks.vue
+++ pages/index/thanks.vue
@@ -1,7 +1,18 @@
 <template>
 	<section class="card">
+		<h3>Thank you for participating!</h3>
+		<p>
+			We hope you love Europe as much as we do. ♥️
+			<br>
+			If you enjoyed this experience, feel free to share this project with your friends and keep in touch!
+		</p>
+		<nav>
+			<a href="https://twitter.com/euinabottle" target="_blank">Twitter</a>
+			<a href="https://instagram.com/euinabottle" target="_blank">Instagram</a>
+			<a href="https://facebook.com/euinabottle" target="_blank">Facebook</a>
+		</nav>
 
-		<nuxt-link to="/" tag="button">Write another bottle.</nuxt-link>		
+		<nuxt-link to="/" tag="button">Write another bottle.</nuxt-link>
 	</section>
 </template>
 
@@ -12,5 +23,8 @@ button {
   margin-top: 24px;
   margin-left: auto;
   margin-right: 0;
+}
+a {
+	margin-right: 8px;
 }
 </style>
